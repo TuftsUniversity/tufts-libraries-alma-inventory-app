d0d03bde7c667ecbd031e84744665860624ba517
Chris Delis <cedelis@uillinois.edu>
2022-06-04 10:54:02 -0500

    Allow individual libraries the ability to override default barcode.init.js values of:
    //Location format message
    var LOC_MSG = "Invalid Location";
    
    //Barcode validation Regular expression
    var BARCODE_REGEX = /^[0-9\-]{5,14}$/;
    
    //Barcode Format message
    var BARCODE_MSG = "Enter a 5-14 digit barcode containing only numbers and dashes";
    
    Simply create a customized file in node/xxx_barcode.init.js with new values, e.g., mbi_barcode.init.js

---
+++ node/mbi_barcode.init.js
@@ -0,0 +1,9 @@
+//Location format message
+LOC_MSG = "Invalid Location";
+
+//Barcode validation Regular expression
+BARCODE_REGEX = /^[0-9\-]{5,14}$/;
+
+//Barcode Format message
+BARCODE_MSG = "Enter a 5-14 digit barcode containing only numbers and dashes (MBI)";
+
