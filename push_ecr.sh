a8d148d03a9d92c6e49df7793da2cc2e7d920287
cedelis <cedelis@uillinois.edu>
2025-05-21 15:53:33 -0500

    new version 0.8

--- push_ecr.sh
+++ push_ecr.sh
@@ -1,3 +1,3 @@
-VER=0.7
+VER=0.8
 docker tag cedelis/alma-inventory-node:${VER} ${AWS_ACCOUNT}.dkr.ecr.us-east-2.amazonaws.com/alma-inventory-node:${VER}
 docker push ${AWS_ACCOUNT}.dkr.ecr.us-east-2.amazonaws.com/alma-inventory-node:${VER}
