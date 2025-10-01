45ef6d8d3c8e0b9e7b3da4f440803efe6c9c00cf
HSteeleLTS <44376066+HSteeleLTS@users.noreply.github.com>
2025-10-01 10:36:16 -0400

    WORKING.  with location dropdown

--- php/Alma.php
+++ php/Alma.php
@@ -1,38 +1,64 @@
 <?php
-//Author: Terry Brady, Georgetown University Libraries
+// Author: Terry Brady, Georgetown University Libraries
+ini_set("log_errors", 1);
+ini_set("error_log", "/var/log/httpd/almainventory_error.log");
+error_log("### Alma.php logging test ###");
+
+
 class Alma {
-	function __construct() {
-      $configpath = parse_ini_file ("Alma.prop", false);
-      $proppath = $configpath["proppath"];
-      $sconfig = parse_ini_file ($proppath, false);
-      $this->alma_apikey = $sconfig["ALMA_APIKEY"];
-	}
 
-  function getApiKey() {
-    return $this->alma_apikey;
-  }
 
-  function getRequest($param) {
-    if (isset($param["apipath"])){
-      $apipath = $param["apipath"];
-      unset($param["apipath"]);
-      $param["apikey"] = $this->getApiKey();
-      $url = "{$apipath}?" . http_build_query($param);
-      $ch = curl_init();
 
-      curl_setopt($ch, CURLOPT_URL,$url);
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-      curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-        'Accept: application/json'
-  	  ));
+    function __construct() {
+        $configpath = parse_ini_file("Alma.prop", false);
+        $proppath   = $configpath["proppath"];
+        $sconfig    = parse_ini_file($proppath, false);
 
-      $jsonstr = curl_exec ($ch);
-
-      curl_close ($ch);
-      echo $jsonstr;
+        $this->alma_apikey      = $sconfig["ALMA_APIKEY"];
+        $this->alma_conf_apikey = $sconfig["ALMA_CONF_APIKEY"];
     }
-    echo "";
-  }
 
+    function getApiKey() {
+        return $this->alma_apikey;
+    }
+
+    function getConfApiKey() {
+        return $this->alma_conf_apikey;
+    }
+
+    function getRequest($param) {
+        if (isset($param["apipath"])) {
+            $apipath = $param["apipath"];
+            unset($param["apipath"]);
+
+            // Use conf API key for ANY /conf/ path
+            if (strpos($apipath, '/conf/') !== false) {
+                $param["apikey"] = $this->getConfApiKey();
+                error_log("Forwarding to CONF API: $apipath");
+            } else {
+                $param["apikey"] = $this->getApiKey();
+                error_log("Forwarding to BIB API: $apipath");
+            }
+
+            // Build URL with the chosen key
+            $url = "{$apipath}?" . http_build_query($param);
+
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'Accept: application/json'
+            ));
+
+            $jsonstr = curl_exec($ch);
+            curl_close($ch);
+
+            echo $jsonstr;
+            return;
+        }
+
+        // No apipath supplied
+        echo "";
+    }
 }
--- php/barcodeReportRedirect.php
+++ php/barcodeReportRedirect.php
@@ -3,12 +3,13 @@
 Author: Terry Brady, Georgetown University Libraries
 Wrapper to pass a request through to the Alma API.  The alma api request will be passed in the param apipath.
 */
-include 'Alma.php';
+include __DIR__ . '/Alma.php';
 
 $ALMA = new Alma();
 header("Content-type: application/json");
 $ALMA->getRequest($_GET);
 // after $url is constructed:
 error_log('Proxy URL: ' . preg_replace('/apikey=[^&]+/', 'apikey=REDACTED', $url));
-
+error_log($url);
 ?>
+
